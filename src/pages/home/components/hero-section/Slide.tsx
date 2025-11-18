85afd32beaeda2ead7c6aa868bc1e1dee494636c
HossamGezo <ha2ghossam10@gmail.com>
2025-11-18 03:07:14 +0200

    feat(hero): complete HeroSection slider with animation and refactored structure

--- src/pages/home/components/hero-section/Slide.tsx
+++ src/pages/home/components/hero-section/Slide.tsx
@@ -1,28 +1,28 @@
+// 
 // Types
 type SlideProps = {
   gradient: string;
   image: string;
   title: string;
   desc: string;
-  translateX: string;
 };
 // Main Component
-const Slide = ({gradient, image, title, desc, translateX}: SlideProps) => {
+const Slide = ({gradient, image, title, desc}: SlideProps) => {
   return (
     <div
-      className={`slide w-screen shrink-0 h-[calc(100vh-129px)] lg:h-[calc(100vh-72.88px)] flex-center flex-col lg:flex-row gap-10 xxl:gap-12 transition-transform duration-500 ${gradient} ${translateX}`}
+      className={`slide w-screen shrink-0 h-[calc(100vh-129px)] lg:h-[calc(100vh-72.88px)] flex-center flex-col lg:flex-row gap-16 ${gradient}`}
     >
-      {/* Slider Image */}
-      <div className="slider-image-container w-[335px] xl:w-[450px] select-none">
+      {/* Slide Image */}
+      <div className="slide-image-container w-[335px] xl:w-[450px] select-none">
         <img
           src={image}
           alt="IMAGE"
-          className="object-contain"
+          className="slide-image object-contain"
           draggable="false"
         />
       </div>
-      {/* Slider Description */}
-      <div className="slider-desc text-[#333] font-serif text-center w-[335px] sm:w-[500px] xl:w-[550px] xxl:w-[900px] select-none">
+      {/* Slide Description */}
+      <div className="slide-desc text-[#333] font-serif text-center w-[335px] sm:w-[500px] xl:w-[550px] xxl:w-[900px] select-none">
         <h3 className="mb-5 text-3xl sm:text-4xl lg:text-6xl xxl:text-[65px] font-bold tracking-wide">
           {title}
         </h3>

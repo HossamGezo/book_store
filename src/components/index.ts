44c5eb1bc0fbf331bfb4b82cfad8fe8dcd96bae2
HossamGezo <ha2ghossam10@gmail.com>
2025-11-22 01:56:05 +0200

    refactor(components): relocate barrel file to ui directory

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./ui/buttons/Button";
-export * from "./ui/inputs/InputField";
-export * from "./ui/rating/Rating";
-export * from "./ui/scroll-to-top/ScrollToTop";
-export * from "./ui/title/Title";
-export * from "./ui/card/Card";

fddf88b721a54fe407bcfcbb1b7f3d9ad5a2f36c
HossamGezo <ha2ghossam10@gmail.com>
2025-11-22 02:20:37 +0200

    feat(home): add MostGifted  section

---
+++ src/pages/home/components/most-gifted/MostGifted.tsx
@@ -0,0 +1,17 @@
+// Data
+import {books} from "../../data/books";
+// Component
+import ProductsSection from "@/components/sections/products-section/ProductsSection";
+// Main Component
+const MostGifted = () => {
+  const mostGiftedBooks = books.sort((a, b) => b.reviews - a.reviews);
+  return (
+    <>
+      <section className="most-gifted py-10 bg-blue-100/25">
+        <ProductsSection title="Most Gifted" sortedArray={mostGiftedBooks} />
+      </section>
+    </>
+  );
+};
+
+export default MostGifted;

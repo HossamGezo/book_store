b56f57aa8505becdb5d236216c69b55d9eda8b7d
HossamGezo <ha2ghossam10@gmail.com>
2026-02-21 17:25:12 +0200

    style: update entry point and implement global CSS utilities and custom scrollbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import {createRoot} from "react-dom/client";
+// Libraries
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 // App Router Component
-import AppRouter from "./routes/AppRouter.tsx";
+import AppRouter from "./routes/AppRouter";
+
 // Styles
 import "./index.css";
+
 // Root
-createRoot(document.getElementById("root")!).render(<AppRouter />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppRouter />
+  </StrictMode>,
+);

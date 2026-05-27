dc9607adba58f5a7f30457adf4d2e121ddfcf9f5
HossamGezo <ha2ghossam10@gmail.com>
2026-05-27 16:00:00 +0300

    refactor: update local data references to point to webp images

--- src/pages/home/data/slides.ts
+++ src/pages/home/data/slides.ts
@@ -5,14 +5,14 @@ export const slides: SlideType[] = [
     id: 1,
     title: "The Books For Everyone",
     description: "You can read at the bookstore or at home",
-    image: "/images/slider/book2.jpg",
+    image: "/images/slider/book2.webp",
     gradient:
       "bg-[linear-gradient(180deg,#165DFB_0%,#2BD8D8_50%,#A6FFFB_100%)]",
   },
   {
     id: 2,
     title: "Book Store",
-    image: "/images/slider/book1.jpg",
+    image: "/images/slider/book1.webp",
     description: "It's not just reading. it's living the adventure",
     gradient:
       "bg-[linear-gradient(180deg,#165DFB_0%,#FF7ED1_50%,#FFD9E8_100%)]",
@@ -21,7 +21,7 @@ export const slides: SlideType[] = [
     id: 3,
     title: "Check Out The New Titles",
     description: "We send you the book you want at home",
-    image: "/images/slider/book3.jpg",
+    image: "/images/slider/book3.webp",
     gradient:
       "bg-[linear-gradient(180deg,#165DFB_0%,#8C6EFF_50%,#DCC9FF_100%)]",
   },
